b9a7ec2e0b61280bd66a9489308b16a5c3227da8
Muskaan-16 <muskaan.sharma@plutustec.com>
2022-07-06 19:19:46 +0530

    updated connection

--- model/connection.js
+++ model/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.ConnectionURL, (result) => console.log("Connected to database on", process.env.ConnectionURL, result))
 
-    // mongoose.connect(`mongodb+srv://muskaan_16:Muskaan_16@cluster0.x6mv0li.mongodb.net/?retryWrites=true&w=majority`, ()=> console.log("Connected to database"))
+    
     .then(() => console.log("connected!"))
     .catch((err) => console.log(err))
 
